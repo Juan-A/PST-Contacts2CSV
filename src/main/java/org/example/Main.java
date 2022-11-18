a85e23172c0b68d83af46f0671cb8cd14aa6fb58
Juan Antonio Herrero <juan13herrero@gmail.com>
2022-11-18 09:39:01 +0100

    Started the project and updated maven dependencies.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("/*******************************/\n" +
+                           "/*     DotServices Software    */\n" +
+                           "/*     --------------------    */\n" +
+                           "/*    Contacts extractor for   */\n" +
+                           "/*            PST Files        */\n" +
+                           "/*(From Inbox and outbox mails)*/\n" +
+                           "/*******************************/");
+    }
+}
