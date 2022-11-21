c03154974e321356b887a850a1d5199578947374
Juan Antonio Herrero <juan13herrero@gmail.com>
2022-11-21 13:48:09 +0100

    Added export CSV method with basic functionality.

--- src/main/java/org/dotServices/Main.java
+++ src/main/java/org/dotServices/Main.java
@@ -1,12 +1,14 @@
 package org.dotServices;
 
+import com.opencsv.CSVWriter;
 import com.pff.PSTException;
 import com.pff.PSTFile;
 import com.pff.PSTFolder;
+import com.pff.PSTMessage;
 
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -58,11 +60,93 @@ public class Main {
             proccessFolder(selectedSubFolder);
         }
         else{
-            System.out.println("No subfolders found.");
+            System.out.println("No subfolders found, do you want to export the records? (y/n)");
+            switch (keyboard.next()){
+                case "y":
+                    System.out.println("Contents of the folder " + selectedSubFolder.getDisplayName());
+                    if(emailsNumber(selectedSubFolder) > 0){
+                        System.out.println("There are " + emailsNumber(selectedSubFolder) + " mails in this folder.");
+                        System.out.println("There are " + selectedSubFolder.getContentCount() + " elements in this folder.");
+                        exportAdresses(selectedSubFolder);
+
+                    }
+                    else{
+                        System.out.println("There are no mails in this folder.");
+                    }
+                    break;
+                case "n":
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Not valid option.");
+                    break;
+            }
         }
 
 
 
+
+    }
+    public static int emailsNumber(PSTFolder pstFolder) throws PSTException, IOException {
+        return pstFolder.getEmailCount();
+    }
+    public static void exportAdresses(PSTFolder pstFolder) throws PSTException, IOException {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Do you want to export the adresses? (y/n): ");
+        switch (keyboard.next()){
+            case "y":
+                System.out.println("Is it an inbox or outbox? (i/o): ");
+                char inboxOrOutbox = keyboard.next().charAt(0);
+                System.out.println("Please enter a name for the file (without extension): ");
+                generateCSV(keyboard.next(), pstFolder, inboxOrOutbox);
+
+                break;
+            case "n":
+                System.out.println("Not exporting adresses...");
+                break;
+            default:
+                System.out.println("Not valid option.");
+                break;
+        }
+    }
+    public static void generateCSV(String fileName, PSTFolder pstFolder, char inboxOrOutbox) throws PSTException, IOException {
+        //creates the csv file utf-8
+        CSVWriter writer = new CSVWriter(new FileWriter(fileName + ".csv"), ',', CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+
+        Map<String,String> mailSenders = new HashMap<>();
+        //creates the record if it doesn't exist
+        PSTMessage message = (PSTMessage) pstFolder.getNextChild();
+        switch (inboxOrOutbox){
+            case 'i':
+                while (message != null) {
+                    if(!mailSenders.containsKey(message.getSenderEmailAddress())){
+                        mailSenders.put(message.getSenderEmailAddress(), message.getSenderName());
+                    }
+                    message = (PSTMessage) pstFolder.getNextChild();
+                }
+                break;
+            case 'o':
+                while (message != null) {
+                    if(!mailSenders.containsKey(message.getEmailAddress())){
+                        mailSenders.put(message.getEmailAddress(), message.getDisplayName());
+                    }
+                    message = (PSTMessage) pstFolder.getNextChild();
+                }
+                break;
+            default:
+                System.out.println("Not valid option.");
+                break;
+        }
+
+        //writes the record to file
+        Iterator<String> it = mailSenders.keySet().iterator();
+        while(it.hasNext()){
+            String key = it.next();
+            String[] record = {key, mailSenders.get(key)};
+            writer.writeNext(record);
+        }
+        writer.close();
+        System.out.println("File generated successfully.");
 
     }
 }
